b443a6e9e17c3bdd94a7a12a211302bcdee23f7a
AChar770 <amandatcharles@yahoo.com>
2026-01-29 13:55:33 -0500

    w3 schools form code added

---
+++ index.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<h2>Events and odds</h2>
+
+<p>Add a number to the bank</p>
+
+<input id="numb">
+
+<button type="button" onclick="myFunctionA()">add number</button>
+
+<button type="button" onclick="myFunctionB()">sort 1</button>
+
+<button type="button" onclick="myFunctionC()">sort all</button>
+
+<p id="demo"></p>
+
+
+
+
+
+<script>
+{/* function myFunction() {
+  // Get the value of the input field with id="numb"
+
+  let x = document.getElementById("numb").value;
+  // If x is Not a Number or less than one or greater than 10
+
+  let text;
+
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    
+    text = "Input OK";
+  }
+  document.getElementById("demo").innerHTML = text;
+} */}
+</script>
+</body>
+</html> 
